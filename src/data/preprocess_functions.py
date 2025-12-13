e5a3b4fcd305215be88cafc8f86243a273a21fc0
Kenia Esmeralda Ramos Javier <133723945+A01799073@users.noreply.github.com>
2025-12-13 17:52:05 +0700

    feat(preprocessing): finalize preprocessing pipeline with CLAHE and gamma correction

--- src/data/preprocess_functions.py
+++ src/data/preprocess_functions.py
@@ -1,7 +1,6 @@
 # Libraries
 import cv2
 import numpy as np
-import os
 
 # Loading images in graysacalde format
 def load_images(path: str) -> np.ndarray:
@@ -20,20 +19,29 @@ def normalize_image(image: np.ndarray)-> np.ndarray:
     image=image.astype(np.float32)/255.0
     return image
 
-# Contrar Enhacement (CLAHE)
-def apply_clahe(image : np.ndarray,clip_limit = 2.0, grid_size=(8,8)) -> np.ndarray:
+# Contrast Enhacement (CLAHE)
+def apply_clahe(image : np.ndarray,clip_limit = 3.0, grid_size=(8,8)) -> np.ndarray:
     # Enhace image constrar using CLAHE, recommended for X-ray.
     clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize = grid_size)
     return clahe.apply(image)
 
+# Gamma correction (enhance dark regions)
+def gamma_correction(image: np.ndarray, gamma: float = 0.8) -> np.ndarray:
+    image = image.astype(np.float32)/255
+    image = np.power(image,gamma)
+    return (image * 255).astype("uint8")
+
 # Full Pipeline
-def preprocess_image(path : str, size=(244,244), use_clahe: bool=True)-> np.ndarray:
-    # Load -> resize -> (CLAHE) -> normalize
+def preprocess_image(path : str, size=(244,244), use_clahe: bool=True, use_gamma: bool = True)-> np.ndarray:
+    # Load -> resize -> (CLAHE) -> Gamma correction -> normalize
     image = load_images(path)
     image = resize_image(image,size)
 
     if use_clahe:
         image = apply_clahe(image)
+
+    if use_gamma:
+        image = gamma_correction(image)
     
     image = normalize_image(image)
 
