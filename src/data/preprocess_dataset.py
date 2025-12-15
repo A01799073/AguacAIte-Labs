351cc4e4053089b8deb19ac3e5310439215d229f
Kenia Esmeralda Ramos Javier <133723945+A01799073@users.noreply.github.com>
2025-12-15 09:04:28 +0700

    feat: update the codes

--- src/data/preprocess_dataset.py
+++ src/data/preprocess_dataset.py
@@ -1,36 +1,48 @@
 import os
 import cv2
+from pathlib import Path
 from typing import Tuple
-from data.preprocess_functions import preprocess_image
 
-RAW_PATH = "data/raw"
-PROCESSED_PATH = "data/processed"
+from src.data.preprocess_functions import preprocess_image
 
-CLASSES = ["normal","turberculosis"]
+RAW_PATH = Path("data/raw")
+PROCESSED_PATH = Path("data/processed")
 
-def preprocess_dataset(raw_dir: str = RAW_PATH, processed_dir: str= PROCESSED_PATH, size: Tuple[int, int] = (244,244), use_clahe:bool = True):
+CLASSES = ["normal","tuberculosis"]
+
+def preprocess_dataset(raw_dir: Path = RAW_PATH, processed_dir: Path= PROCESSED_PATH, size: Tuple[int, int] = (244,244), use_clahe:bool = True):
     """
     Applies the method of processing all images in raw_dir and save it into "processed_dir"
     in order to preserve class foder structures
     """
 
     for cls in CLASSES:
-        input_dir = os.path.join(raw_dir, cls)
-        output_dir = os.path.join(processed_dir, cls)
+        input_dir =  raw_dir / cls
+        output_dir = processed_dir / cls
 
-        os.makedirs(output_dir,exist_ok=True)
+        output_dir.mkdir(parents=True, exist_ok=True)
 
         print(f"\nProcessing class: {cls}")
 
-        for filename in os.listdir(input_dir):
-            input_path = os.path.join(input_dir, filename)
-            output_path = os.path.join(output_dir,filename)
+        for img_name in os.listdir(input_dir):
+            img_path = input_dir / img_name
 
-            try: 
-                image = preprocess_image(path=input_path,size=size, use_clahe=use_clahe)
-                # uint8 image (0-255) for compatibility
-                image_uint8 = (image * 255).astype("uint8")
-                cv2.imwrite(output_path, image_uint8)
+            try:
+                img = preprocess_image(
+                    path=str(img_path),
+                    size=size,
+                    use_clahe=use_clahe
+                )
+
+                # Convert back to uint8 for saving
+                img_uint8 = (img * 255).astype("uint8")
+                cv2.imwrite(str(output_dir / img_name), img_uint8)
 
             except Exception as e:
-                print(f"[ERROR] {filename}: {e}")
+                print(f"[ERROR] {img_name}: {e}")
+
+    print("✅ Dataset preprocessing completed")
+
+
+if __name__ == "__main__":
+    preprocess_dataset()
