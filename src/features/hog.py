5e7f3301571f25ea07ae33b0f8c6bb9d9bafa889
Kenia Esmeralda Ramos Javier <133723945+A01799073@users.noreply.github.com>
2026-01-03 22:36:37 +0700

    feat: Implementation HOG Feature

---
+++ src/features/hog.py
@@ -0,0 +1,33 @@
+import numpy as np
+from skimage.feature import hog
+from skimage.color import rgb2gray
+
+
+def extract_hog_features(image, pixels_per_cell=(16, 16),cells_per_block=(2, 2), orientations=9):
+    """
+    Compute HOG features for a single image.
+    """
+
+    if image.ndim == 3:
+        image = rgb2gray(image)
+
+    return hog(
+        image,
+        orientations=orientations,
+        pixels_per_cell=pixels_per_cell,
+        cells_per_block=cells_per_block,
+        block_norm="L2-Hys",
+        feature_vector=True
+    )
+
+
+def extract_hog_dataset(images, **hog_params):
+    """
+    Compute HOG features for a collection of images.
+    """
+
+    features = []
+    for img in images:
+        features.append(extract_hog_features(img, **hog_params))
+
+    return np.array(features)
